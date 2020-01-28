40c13165ea88d535ec28278acb49c2dc9ab6a185
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-28 06:06:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,36 +1,54 @@
 class Artist
-  @@all = []
-  attr_accessor :name, :songs
-  
-  def initialize(name)
+attr_accessor :name, :songs
+
+  @@all = [] #array of artists instances
+
+  def initialize(name)#artist_name
     @name = name
-    @songs = []
-    @@all << self
+    @songs = [] #array of songs for each instance
   end
-  
-  def add_song(song)
-   @songs << song
-  end
-   
+
+  #this exposes the @@all Class Variable
   def self.all
     @@all
   end
-  
-  def songs
-    Song.all.select {|s| s.artist == self}
-  end
-  
-  def self.find_or_create_by_name(string_name)
-    if Song.all.any?{|n| n.name = string_name}
-      @name = string_name
-    else
-      Artist.new(string_name)
-    end
-  end
-  
-  def print_songs
-    puts self.songs
+
+  def add_song(song)
+    #called by the song instance, adds in that song instance
+    #called inside the song method #artist_name=
+    #after the song has been created, assigned a name
+    #the song class checks to see if there is an artist associated with the song, and if there isnt' one, it asks the Artist class to create an artist instance
+    #it then passes that artist instance back to the song to store as an attribute
+    #the song class then calls this method #add_song
+    #this method adds the song to the artist instances song array
+    #every artist instance has this array
+    @songs << song
   end
 
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+    @@all << artist
+    artist
+  end
+
+  def save
+    @@all << self
+  end
+  
 end
 
