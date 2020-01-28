027a5914c5c8b451596d499e00aa82b5666ef85d
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-28 06:14:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,10 +25,7 @@ class Song
     #This method will do two things. Both of these things will involve collaboration with the Artist class:Turn the artist's name as a string into an Artist object. First we need to get the instance of the Artist class that represents that artist. There are two possibilities here: Either we have to create that Artist instance Or it already exists and we have to find that Artist instance. To achieve this, we'll need to collaborate with the Artist class. We want to send an artist's name (a string--remember we are getting this from the parsed filename) to the Artist class to achieve the functionality described above in #1 and #2. This sounds like a great place to use method Artist.find_or_create_by_name. Assign the song to the artist (Since an artist has many songs, we'll want to make this association) Now that we have the artist instance, we'll want to again collaborate with the Artist class by calling on the Artist#add_song(some_song) method.
 =end
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
+   
     end
   end
   
